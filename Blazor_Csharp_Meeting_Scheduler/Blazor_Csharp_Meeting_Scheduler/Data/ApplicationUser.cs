1766df78b245f03d1d4697d1cf5cb477812fe7e9
Robert McCabe <rmccabe1023@gmail.com>
2024-09-02 14:51:39 -0400

    update file

--- Blazor_Csharp_Meeting_Scheduler/Blazor_Csharp_Meeting_Scheduler/Data/ApplicationUser.cs
+++ Blazor_Csharp_Meeting_Scheduler/Blazor_Csharp_Meeting_Scheduler/Data/ApplicationUser.cs
@@ -5,6 +5,7 @@ namespace Blazor_Csharp_Meeting_Scheduler.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        
     }
 
 }
