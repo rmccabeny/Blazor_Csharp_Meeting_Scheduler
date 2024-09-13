34c91ee9442f711aa09a2919a058962a8abbdc56
Robert McCabe <rmccabe1023@gmail.com>
2024-09-12 20:32:01 -0400

    update file

--- Blazor_Csharp_Meeting_Scheduler/Blazor_Csharp_Meeting_Scheduler/Models/Meeting.cs
+++ Blazor_Csharp_Meeting_Scheduler/Blazor_Csharp_Meeting_Scheduler/Models/Meeting.cs
@@ -4,13 +4,14 @@
     {
         // establish the properties of the meeting class
         public int MeetingId { get; set; }
-        public required string MeetingTitle { get; set; } = string.Empty;
+        public required string MeetingTitle { get; set; } 
         public int DepartmentId { get; set; }
-        public required string DepartmentName { get; set; } = string.Empty;
-        public required string EmployeeName { get; set; } = string.Empty;
-        public required string EmployeeEmail { get; set; } = string.Empty;
-        public required string EmployeePhone { get; set; } = string.Empty;
-        public required string ManagerName { get; set; } = string.Empty;
+        public required string DepartmentName { get; set; } 
+        public int EmployeeId { get; set; }
+        public required string EmployeeName { get; set; } 
+        public required string EmployeeEmail { get; set; } 
+        public required string EmployeePhone { get; set; } 
+        public required string ManagerName { get; set; } 
         public required DateTime MeetingDateTime { get; set; }
         
         
