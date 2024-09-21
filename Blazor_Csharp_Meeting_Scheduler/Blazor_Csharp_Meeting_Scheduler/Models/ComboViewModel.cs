e7ef9b61612f28f8f13af56a39452094d71b9268
Robert McCabe <rmccabe1023@gmail.com>
2024-09-21 16:20:46 -0400

    update files

--- Blazor_Csharp_Meeting_Scheduler/Blazor_Csharp_Meeting_Scheduler/Models/ComboViewModel.cs
+++ Blazor_Csharp_Meeting_Scheduler/Blazor_Csharp_Meeting_Scheduler/Models/ComboViewModel.cs
@@ -2,10 +2,12 @@
 {
     public class ComboViewModel
     {
+        public int EmployeeId { get; set; }
         public required string EmployeeName { get; set; }
         public required string DepartmentName { get; set; }
         public required string EmployeeEmail { get; set; }
         public required string EmployeePhone { get; set; }
+        public int MeetingId { get; set; }
         public required string MeetingTitle { get; set; }
         public required DateTime MeetingDateTime{ get; set; }
         
